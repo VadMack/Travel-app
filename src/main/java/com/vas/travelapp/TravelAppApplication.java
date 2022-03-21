c71d65dbb678c81d880ffd39d56a95fc405d9be6
VadMack <makarovvadim01@icloud.com>
2022-03-21 12:41:55 +0300

    Initial

---
+++ src/main/java/com/vas/travelapp/TravelAppApplication.java
@@ -0,0 +1,14 @@
+package com.vas.travelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAppApplication.class, args);
+        System.out.println("Aboba");
+    }
+
+}
