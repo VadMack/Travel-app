c2c6297075e04f4ec6510c1867804675046a3f2a
VadMack <makarovvadim01@icloud.com>
2022-03-29 20:52:03 +0300

    #5: Swagger

---
+++ src/main/java/com/vas/travelapp/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.vas.travelapp.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${app-version}") String version) {
+        System.out.println("B");
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Travel-app")
+                        .version(version)
+                        .description("Coarse work №3")
+                );
+    }
+}
