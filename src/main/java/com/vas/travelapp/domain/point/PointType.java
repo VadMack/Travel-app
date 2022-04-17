cc9d20f7bdb5b410aa53b466c9501fd3844d9534
Alexey Gerasimov <aagerasimov_3@edu.hse.ru>
2022-04-17 17:26:46 +0300

    Initial Point Model created (#12)
    * Initial Point Model created
    
    * Point Model created
    
    * restructuring
    
    Co-authored-by: Stanislav Stoianov <o_ssstoyanov@sportmaster.ru>

---
+++ src/main/java/com/vas/travelapp/domain/point/PointType.java
@@ -0,0 +1,13 @@
+package com.vas.travelapp.domain.point;
+
+public enum PointType {
+    THEATER,
+    CASINO,
+    CAFE,
+    BAR,
+    PARK,
+    RESTAURANT,
+    MUSEUM,
+    GALLERY,
+    EVENT
+}
