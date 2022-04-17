299090cb9de7eec2d8c4c4b1b4f4d6c0eba846be
Stanislav Stoianov <o_ssstoyanov@sportmaster.ru>
2022-04-17 17:40:35 +0300

    added route

---
+++ src/main/java/com/vas/travelapp/api/mappers/RouteMapper.java
@@ -0,0 +1,11 @@
+package com.vas.travelapp.api.mappers;
+
+import com.vas.travelapp.api.dtos.RouteDto;
+import com.vas.travelapp.domain.route.Route;
+
+public class RouteMapper {
+    // todo: implement
+    public RouteDto toDto(Route route) {
+        return null;
+    }
+}
---
+++ src/main/java/com/vas/travelapp/domain/route/RouteService.java
@@ -0,0 +1,13 @@
+package com.vas.travelapp.domain.route;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RouteService {
+    // todo: implement
+    public Route getRoute() {
+        return null;
+    }
+}
