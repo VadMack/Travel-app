e72bc08a13f7cdaab607a12331f03a08d44cd57e
Alexey Gerasimov <gerasimov2122@icloud.com>
2022-06-01 23:33:13 +0300

    Update PointService.java

--- core/src/main/java/com/vas/travelapp/domain/point/PointService.java
+++ core/src/main/java/com/vas/travelapp/domain/point/PointService.java
@@ -35,36 +35,6 @@ public class PointService {
         return pointRepository.save(point);
     }
 
-    /*public void generateData() {
-        ArrayList<Point> points = new ArrayList<>(150);
-        for (int i = 0; i < 150; i++) {
-            Point point = new Point();
-            point.setName("Point " + i);
-            point.setType(PointType.values()[i % PointType.values().length]);
-            point.setPrice(Price.values()[i % Price.values().length]);
-            Address address = new Address();
-            address.setStreetAddress("Maiakovski st. " + i);
-            address.setCity("Tiraspol");
-            address.setCountry("MD");
-            address.setZipCode(3300);
-            address.setLatitude(46.852624 + (double) i / 100);
-            address.setLongitude(29.630269 + (double) i / 100);
-            point.setAddress(address);
-
-            List<OperationHours> operationHours = new ArrayList<>();
-            for (int j = 0; j < 6; j++) {
-                operationHours.add(new OperationHours().setDayOfWeek(DayOfWeek.values()[i % DayOfWeek.values().length])
-                        .setOpeningTime(LocalTime.of(8, 0, 0))
-                        .setCloseTime(LocalTime.of(20, 0, 0))
-                        .setPoint(point));
-            }
-            operationHours.add(new OperationHours().setDayOfWeek(DayOfWeek.SUNDAY).setClosed(true).setPoint(point));
-            point.setOperationHours(operationHours);
-            points.add(point);
-        }
-        pointRepository.saveAll(points);
-    }*/
-
     public List<Point> generateRoute(Preferences preferences) {
         TypeActivity activity = preferences.getActivity();
         RouteStrategy routeStrategy = null;
