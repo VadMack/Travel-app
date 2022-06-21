6dd2fc10002bfc3f4de4285e63fcb9193434fd3e
Alexey Gerasimov <gerasimov2122@icloud.com>
2022-06-21 11:37:09 +0300

    Update SwaggerConfig.java

--- core/src/main/java/com/vas/travelapp/config/SwaggerConfig.java
+++ core/src/main/java/com/vas/travelapp/config/SwaggerConfig.java
@@ -28,7 +28,6 @@ public class SwaggerConfig {
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.vas.travelapp.api.controllers"))
                 .paths(PathSelectors.any())
                 .build();
     }
